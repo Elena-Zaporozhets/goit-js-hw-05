8ab92528b88ed1a64006bfe69b9cef7d97aeaf0d
Elena-Zaporozhets <elenazaporozhec28@gmail.com>
2026-02-04 00:13:57 +0200

    fix

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
 const getTotalBalanceByGender = (users, gender) => {
     return users
         .filter(user => user.gender === gender)
-        .reduce((acc, user) => { acc + user.balance; }, 0);
+        .reduce((acc, user) =>  acc + user.balance, 0);
 }
 
 
